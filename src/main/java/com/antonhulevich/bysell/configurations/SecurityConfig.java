308943ac1546e4bf848f1d8cc867e66240734baf
Anton Hulevich <antonhulevich@gmail.com>
2025-09-10 16:35:57 +0300

    add AdminController add method userInfo in UserController add method userList in UserService add method isAdmin in User add view admin.ftlh add view user-info.ftlh add view user-edit.ftlh

--- src/main/java/com/antonhulevich/bysell/configurations/SecurityConfig.java
+++ src/main/java/com/antonhulevich/bysell/configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.antonhulevich.bysell.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
@@ -21,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/product/**","/images/**","/registration/**").permitAll()
+                        .requestMatchers("/", "/product/**","/images/**","/registration/**","/user/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
